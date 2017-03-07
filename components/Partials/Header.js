09fbc3755c2058d9fd123a22e9c26032d2617a04
david hurley <davidhurley@davids-Air.workhaus.bz>
2017-03-07 11:57:09 -0500

    add cool dude nav

--- components/Partials/Header.js
+++ components/Partials/Header.js
@@ -11,20 +11,8 @@ export default class Header extends Component {
     const subheadline = data.page.subheadline
     
     return (
-      <div>
-        <header className="intro-header" style={ { backgroundImage: "url('" + hero + "')" } }>
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-                <div className="site-heading">
-                  <h1>headline { headline }</h1>
-                  <hr className="small" />
-                  <span className="subheading">{ subheadline }</span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </header>
+      <div className='header'>
+        <h1>{ headline }</h1>
       </div>
     )
   }
--- components/Partials/Nav.js
+++ components/Partials/Nav.js
@@ -5,9 +5,18 @@ import { Link } from 'react-router'
 export default class Nav extends Component {
 
   handleClick(){
-    $('.navbar-collapse').removeClass('in')
-    $('html,body').scrollTop(0)
+    $('.navOverlay').hide();
   }
+
+  closeMe(){
+    $('.navOverlay').hide();
+  }
+
+  showMe() {
+    $('.navOverlay').show();
+  }
+
+
 
   render(){
     
@@ -28,25 +37,28 @@ export default class Nav extends Component {
     })
 
     return (
-      <nav className="navbar navbar-default navbar-custom navbar-fixed-top">
-        <div className="container-fluid">
-          <div className="navbar-header page-scroll">
-            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-              <span className="sr-only">Toggle navigation</span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-            </button>
-            <a className="navbar-brand" href="/">Mike Rowes blog</a>
+      <div>
+        <nav className="navbar navbar-default navbar-custom navbar-fixed-top">
+          <div className="container-fluid">
+            <div className="navbar-header page-scroll">
+              <button onClick={ this.showMe } type="button" className="navbar-toggle">
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
+              <a className="navbar-brand" href="/">Apologue</a>
+            </div>
           </div>
-
-          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul className="nav navbar-nav navbar-right">
+        </nav> 
+        <div className='navOverlay'>
+            <ul >
               { menu_items }
             </ul>
-          </div>
+            <span onClick={ this.closeMe } className='closeMe'>close me</span>
         </div>
-      </nav>
+      </div>
+
     )
   }
 }
